7b817e1d68e9bb4cab72ce7d651eca0fae5b8836
AmagiSakuya <AmagiSakuya@163.com>
2026-01-27 21:08:36 +0800

    add DS5  and Pro Controller

--- src/controllerDefinition.js
+++ src/controllerDefinition.js
@@ -13,6 +13,32 @@ module.exports = [
         }
     },
     {
+        deviceName: "DualSense 5",
+        vid: 1356,
+        pid: 3302,
+        parseDataFunction: function (buffer) {
+            let LEFT_SHOULDER = (buffer[9] & 0x01) !== 0;  // L1
+            let RIGHT_SHOULDER = (buffer[9] & 0x02) !== 0; // R1
+            let SELECT = (buffer[9] & 0x10) !== 0;         // Create (原 Share)
+            let START = (buffer[9] & 0x20) !== 0;          // Options
+            let HOME = (buffer[10] & 0x01) !== 0;           // PS Button
+            return { LEFT_SHOULDER, RIGHT_SHOULDER, SELECT, START, HOME };
+        }
+    },
+    {
+        deviceName: "Nintendo Switch Pro Controller",
+        vid: 0x057e,
+        pid: 0x2009,
+        parseDataFunction: function (buffer) {
+            let LEFT_SHOULDER = (buffer[5] & 0x40) !== 0;
+            let RIGHT_SHOULDER = (buffer[3] & 0x40) !== 0;
+            let SELECT = (buffer[4] & 0x01) !== 0;
+            let START = (buffer[4] & 0x02) !== 0;
+            let HOME = (buffer[4] & 0x10) !== 0;
+            return { LEFT_SHOULDER, RIGHT_SHOULDER, SELECT, START, HOME };
+        }
+    },
+    {
         deviceName: "XInput (网易UU远程)",
         vid: 1118,
         pid: 654,
